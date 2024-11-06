f8b9fbe97fd27fcb63f57fb20388dc1d50549984
Kochi Masahiro <workspace2110@gmail.com>
2024-11-06 17:37:25 +0800

    Fix known issue: crypt will be removed in python3.13
    Reference: https://docs.python.org/3/whatsnew/3.13.html

--- hash_password.py
+++ hash_password.py
@@ -1,10 +1,9 @@
-from crypt import crypt
-from crypt import mksalt
-from crypt import METHOD_SHA512
+import string
+import random
 from getpass import getpass
-
 from optparse import OptionParser
 
+from passlib.hash import sha512_crypt
 from password_strength import PasswordPolicy
 
 
@@ -26,6 +25,17 @@ def _get_parameters() -> OptionParser:
     return parser
 
 
+def gen_salt(length: int = 16) -> str:
+    character_list = string.ascii_letters
+    character_list += string.digits
+    salt = []
+    for _ in range(length):
+        random_char = random.choice(character_list)
+        salt.append(random_char)
+    
+    return "".join(salt)
+
+
 def main():
     # Load parameters
     parser = _get_parameters()
@@ -39,8 +49,8 @@ def main():
     if pwd is None:
         print("Please input password")
         pwd = getpass()
-
-    print("Weak Password" if policy.test(pwd) else f"Hashed value: {crypt(pwd, mksalt(METHOD_SHA512))}")
+    salt = gen_salt()
+    print("Weak Password" if policy.test(pwd) else f"Hashed value: {sha512_crypt.using(salt=salt, rounds=5000).hash(pwd)}")
 
 
 if __name__ == "__main__":
